f8bf2ec5258b58f0e032ce963f90a0b6dad73f2c
sab148 <benassou.narimene@gmail.com>
2021-10-18 00:20:34 +0100

    mod

--- wsol/layers/__init__.py
+++ wsol/layers/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 from .batch_norm import FrozenBatchNorm2d, get_norm, NaiveSyncBatchNorm
 #from .deform_conv import DeformConv, ModulatedDeformConv
-from .mask_ops import paste_masks_in_image
+#from .mask_ops import paste_masks_in_image
 from .nms import batched_nms, batched_nms_rotated, nms, nms_rotated
 from .roi_align import ROIAlign, roi_align
 #from .roi_align_rotated import ROIAlignRotated, roi_align_rotated
