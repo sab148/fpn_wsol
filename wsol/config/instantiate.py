923b86da6a4f7dd41f4275311621a1aa39a28e18
sab148 <benassou.narimene@gmail.com>
2021-10-17 22:48:33 +0100

    mod

--- wsol/config/instantiate.py
+++ wsol/config/instantiate.py
@@ -4,7 +4,7 @@ import logging
 from collections import abc
 from typing import Any
 
-from detectron2.utils.registry import _convert_target_to_string, locate
+from utils.registry import _convert_target_to_string, locate
 
 __all__ = ["dump_dataclass", "instantiate"]
 
