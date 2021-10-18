0393ae561961e8cb67b91d2c1ef42d11304eca07
sab148 <benassou.narimene@gmail.com>
2021-10-18 01:18:42 +0100

    mod

--- model_zoo/detection_checkpoint.py
+++ model_zoo/detection_checkpoint.py
@@ -6,9 +6,9 @@ import torch
 from fvcore.common.checkpoint import Checkpointer
 from torch.nn.parallel import DistributedDataParallel
 
-import detectron2.utils.comm as comm
-from detectron2.utils.env import TORCH_VERSION
-from detectron2.utils.file_io import PathManager
+import wsol.utils.comm as comm
+from wsol.utils.env import TORCH_VERSION
+from wsol.utils.file_io import PathManager
 
 from .c2_model_loading import align_and_update_state_dicts
 
